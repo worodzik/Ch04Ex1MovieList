e1557f288fac0858950918ec9e34087f8bd66732
Worod Zik <worodzik@yahoo.com>
2022-03-22 01:59:38 -0400

    Add project files.

---
+++ Migrations/20220322052809_Genre.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieList.Migrations
+{
+    public partial class Genre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GenreId",
+                table: "Movies",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    GenreId = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.GenreId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genre",
+                columns: new[] { "GenreId", "Name" },
+                values: new object[,]
+                {
+                    { "A", "Action" },
+                    { "C", "Comedy" },
+                    { "D", "Drama" },
+                    { "H", "Horror" },
+                    { "M", "Musical" },
+                    { "R", "RomCom" },
+                    { "S", "SciFi" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 1,
+                column: "GenreId",
+                value: "D");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 2,
+                column: "GenreId",
+                value: "A");
+
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 3,
+                column: "GenreId",
+                value: "R");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_GenreId",
+                table: "Movies",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Genre_GenreId",
+                table: "Movies",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "GenreId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Genre_GenreId",
+                table: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_GenreId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "GenreId",
+                table: "Movies");
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MovieList.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889, 2050, ErrorMessage = "Year must be between after 1889.")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int? Rating { get; set; }
+
+        [Required(ErrorMessage = "Please enter a genre.")]
+        public String GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+        public string Slug =>
+            Name?.Replace(' ', '-').ToLower() + '-' + Year?.ToString();
+    }
+}
